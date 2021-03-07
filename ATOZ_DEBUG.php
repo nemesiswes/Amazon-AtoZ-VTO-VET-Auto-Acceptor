f5a1d2d67ced0fc1e83e77ea97613e9402e93983
Wesley Waid <nemesiswes@gmail.com>
2021-03-06 21:12:17 -0800

    Create ATOZ_DEBUG.php

---
+++ ATOZ_DEBUG.php
@@ -0,0 +1,6 @@
+<?php
+
+VTO Spoof
+echo '{"vtoOpportunities":[{"start_time":"2021-02-13T09:20:00.000Z","start_time_local":"2021-02-13T01:20:00","end_time":"2021-02-13T19:50:00.000Z","end_time_local":"2021-02-13T11:50:00","signup_start_time":"2021-02-13T08:46:00.000Z","signup_start_time_local":"2021-02-13T00:46:00","signup_end_time":"2021-02-14T09:10:00.000Z","signup_end_time_local":"2021-02-14T01:10:00","site_time_zone":null,"opportunity_id":"amzn.ls.opportunityid915a7eb9-7a9b-4409-8817-35dbb0b20309","workgroup":"Outbound","active":true,"opportunity_type":"VTO","inactive_reason":"","minutes_to_cover_opportunity":600,"minimum_time_denomination_in_minutes":30,"accrual_balances":[],"accrual_supported":false,"is_opportunity_incentivized":false,"incentives":null,"droppable":false,"drop_start_time":null,"drop_start_time_local":null,"drop_end_time":null,"drop_end_time_local":null}],"vetOpportunities":[]}';
+
+?>
